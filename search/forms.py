da04b0c171b60abd5ac963f77ecc73d311d6820b
keayantsuro <i-miyagi@xj9.so-net.ne.jp>
2025-01-23 17:27:50 +0900

    bootstrapのナビバ更新

--- search/forms.py
+++ search/forms.py
@@ -2,7 +2,18 @@ from django import forms
 
 class SearchForm(forms.Form):
     keywords = forms.CharField(label="", required=False, max_length=150,
-        widget=forms.TextInput(attrs={'placeholder': '入力してください'}))
+        widget=forms.TextInput(attrs={
+            'class': 'form-control me-2',
+            'type': 'search',
+            'placeholder': '名前や都道府県名など',
+            'aria-label': 'Search',
+        })
+    )
 
 class UploadForm(forms.Form): # アップロードフォーム
-    file = forms.FileField(required=True, label='')
+    file = forms.FileField(required=True, label='',
+        widget=forms.ClearableFileInput(attrs={
+            'class': 'form-control',
+            'id': 'customFile',
+        })
+    )
